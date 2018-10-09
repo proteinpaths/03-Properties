c21c6fe2906061343cb7483b758d4816f86488bf
Luis Garreta <lgarreta@uao.edu.co>
2018-10-09 15:52:05 -0500

    UPD ss

--- dev/metrics/xStructuralSimilarity/structural_similarity.py
+++ dev/metrics/xStructuralSimilarity/structural_similarity.py
@@ -3,7 +3,7 @@
 USAGE  = "Calculate the structural similarity between structures using structural alphabets"
 USAGE += "USAGE: structural.py <Target PDB >  <Reference PDB> [outputFilename]\n"
 
-import os, sys
+import os, sys, subprocess
 
 ###############################################################################
 # Returns the stem BASENAME  of a relative or full name with some extension
@@ -48,20 +48,17 @@ os.system ("mkdir %s" % tmpDir)
 outputRef = "%s/tmp-%s.output" % (tmpDir, name (pdbRef))
 logRef = "%s/tmp-%s.log" % (tmpDir, name (pdbRef))
 
-params = {"IN": pdbRef, "ALPHABET": alphabet, "OUT": outputRef, "LOG": logRef }
-cmm2 =  "encoder -g -F %(IN)s -A %(ALPHABET)s -T l -O %(OUT)s &> %(LOG)s" % params
-#print (cmm2)
-os.system (cmm2)
+params = {"IN": pdbRef, "ALPHABET": alphabet, "OUT": outputRef}
+cmm =  "encoder -g -F %(IN)s -A %(ALPHABET)s -T l -O %(OUT)s" % params
+out1,err1 = subprocess.Popen (cmm.split(), stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=tmpDir).communicate()
 
 # Process the target 
 outputTarget = "%s/tmp-%s.output" % (tmpDir, name (pdbTarget))
 logTarget = "%s/tmp-%s.log" % (tmpDir, name (pdbTarget))
 
-params = {"IN": pdbTarget, "ALPHABET": alphabet, "OUT": outputTarget, "LOG": logTarget }
-cmm1 =  "encoder -g -F %(IN)s -A %(ALPHABET)s -T l -O %(OUT)s &> %(LOG)s" % params
-#print (cmm1)
-
-os.system (cmm1)
+params = {"IN": pdbTarget, "ALPHABET": alphabet, "OUT": outputTarget}
+cmm =  "encoder -g -F %(IN)s -A %(ALPHABET)s -T l -O %(OUT)s" % params
+out2,err2 = subprocess.Popen (cmm.split(), stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=tmpDir).communicate()
 
 # Get the outputs
 encodeTarget = open (outputTarget).readlines()[2]
@@ -72,6 +69,6 @@ equals = len (filter (lambda x:x[0]==x[1], zip (encodeTarget, encodeRef)))
 proportion = equals / float (len (encodeRef))
 
 # Clean temporary files
-clean ([outputTarget, outputRef, logTarget, logRef, tmpDir])
+clean ([outputTarget, outputRef, tmpDir])
 print proportion
 
